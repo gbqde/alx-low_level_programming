ab4f91c2ef99805775681cb5ef54c3987a6ca76e
Gbade Moses <122960586+gbqde@users.noreply.github.com>
2023-02-17 03:05:41 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
  * Return: 0 (Success)
  */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+{puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
