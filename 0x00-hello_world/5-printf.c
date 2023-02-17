a5c0ff26e43a1bb20b96a1b037ba019b7d440875
Gbade Moses <houser.inc@gmail.com>
2023-02-17 03:45:23 +0100

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints using the f function
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
