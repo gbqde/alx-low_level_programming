ab0579e6238dc299c11d581bd5fc9f0d21feda6b
Gbade Moses <houser.inc@gmail.com>
2023-02-18 21:58:05 +0100

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,17 +14,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
 	else if (n < 0)
-	{
-		printf("%d is negative", n);
-	}
+		printf("%d is negative", n);}
 	else
-	{
 		printf("%d is zero\n", n);
-	}
+	
 
 	return (0);
 }
