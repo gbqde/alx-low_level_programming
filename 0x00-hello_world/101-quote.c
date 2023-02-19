9fbc395b076646d6dc519351937e5b83fc577b38
Gbade Moses <houser.inc@gmail.com>
2023-02-19 02:12:19 +0100

    for 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,2 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
- *main - c program that prints a line to the standard error
- *Return: 1 (success)
- */
-int main(void)
-{
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
-}
+#!/bin/bash
+gcc -Wall -w -o /dev/stderr -x c - <<<'int main(void) {return write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);}'
