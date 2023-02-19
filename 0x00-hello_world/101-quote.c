00cbb7c86f74528f3848f99d69145b5b8b23c892
Gbade Moses <houser.inc@gmail.com>
2023-02-19 02:16:07 +0100

    for 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -Wall -w -o /dev/stderr -x c - <<<'int main(void) {return write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);}'
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - Entry point
+ *Return: 1 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
