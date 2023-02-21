80c0744b5c57e2d4088ef7b8bcff85dd5535836d
Gbade Moses <houser.inc@gmail.com>
2023-02-21 13:06:07 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,18 @@
 /**
  * print_alphabet_x10 - prints 10x the alphabet
  */
-
 void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
 
-	while (count++ <= 9)
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+	while (count < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+		count++;
+	}
 }
