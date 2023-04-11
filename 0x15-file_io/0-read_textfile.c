e7a0d583d6a26faf9312312be890ea070c4c65db
Gbade Moses <houser.inc@gmail.com>
2023-04-11 03:17:44 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,9 +9,7 @@
  * Return: the number of letters it could read and print
  *         0 if the file can not be opened or read, or if filename is NULL
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
 	int fd, num_read, num_written;
 	char *buf;
@@ -33,17 +31,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buf);
 		close(fd);
 		return (0);
+	}
+
+	num_written = write(STDOUT_FILENO, buf, num_read);
+	if (num_written == -1 || num_written != num_read)
 	{
-		num_written = write(STDOUT_FILENO, buf, num_read);
-		if (num_written == -1 || num_written != num_read)
-
-		{
-			free(buf);
-			close(fd);
-			return (0);
-		}
-
 		free(buf);
 		close(fd);
-		return (num_written);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+	return (num_written);
 }
